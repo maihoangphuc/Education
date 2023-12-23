4f3456d44309a31b67f271dc30a6d31e3ce4c9af
maihoangphuc <maihoangphuc9x@gmail.com>
2023-12-23 23:57:02 +0700

    validate form signin

--- Education/Areas/Admin/Controllers/AuthController.cs
+++ Education/Areas/Admin/Controllers/AuthController.cs
@@ -6,6 +6,10 @@ namespace Education.Areas.Admin.Controllers
     [Area("Admin")]
     public class AuthController : Controller
     {
+        private static readonly string Username = "admin";
+        private static readonly string Password = "12345678";
+
+        [HttpGet]
         public IActionResult SignIn()
         {
             return View();
@@ -13,18 +17,19 @@ namespace Education.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult SignIn(LoginModel model)
+        public IActionResult SignIn(AccountModel model)
         {
             if (ModelState.IsValid)
             {
-                if (model.Username.ToLower() == "admin" && model.Password.ToLower() == "12345678")
+                if (model.Username.ToLower() == Username && model.Password.ToLower() == Password)
                 {
                     HttpContext.Session.SetString("user", "admin");
                     return RedirectToAction("index", "home", new { area = "admin" });
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Tên người dùng hoặc mật khẩu không hợp lệ");
+                    // Nếu thông tin đăng nhập không đúng, thêm thông báo lỗi vào ModelState
+                    ModelState.AddModelError("Username", "Tên người dùng hoặc mật khẩu không đúng.");
                 }
             }
 
@@ -34,7 +39,7 @@ namespace Education.Areas.Admin.Controllers
         public IActionResult SignOut()
         {
             HttpContext.Session.Remove("user");
-            return RedirectToAction("signin");
+            return RedirectToAction("signin", "auth", new { area = "admin" });
         }
     }
 }
--- Education/Models/AuthModel.cs
+++ Education/Models/AuthModel.cs
@@ -1,12 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Education.Models
 {
-    public class LoginModel
+    public class AccountModel
     {
+        [DisplayName("Tên tài khoản")]
         [Required(ErrorMessage = "Tên người dùng là bắt buộc.")]
         public string Username { get; set; }
 
+        [DisplayName("Mật khẩu")]
         [Required(ErrorMessage = "Mật khẩu là bắt buộc.")]
         [MinLength(8, ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự.")]
         public string Password { get; set; }
